b9c8a59a99520afb9111e88d13316b10b4f933ca
kwatch <kwatch@gmail.com>
2021-10-09 18:33:03 +0900

    python: update 'setup.py'

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def _kwargs():
     version       = '$Release: 0.0.0 $'.split(' ')[1]
     author        = 'kwatch'
     author_email  = 'kwatch@gmail.com'
-    description   = 'Utility to download files from CDNJS, jsDelivr or Google'
+    description   = 'Utility to download files from public CDN (CDNJS/jsDelivr/UNPKG/Google)'
     url           = 'https://github.com/kwatch/cdnget/tree/python'
     download_url  = _pypi_download_url(name, version)
     license       = 'MIT'
@@ -23,8 +23,13 @@ def _kwargs():
     py_modules    = ['cdnget']
     #package_dir   = {'': PY2 and 'lib2' or 'lib3'}
     #packages     = ['cdnget']
-    scripts       = ['bin/cdnget']
+    #scripts       = ['bin/cdnget']
     #install_requires = ['oktest']
+    entry_points  = {
+        "console_scripts": [
+            "cdnget = cdnget:main",
+        ],
+    }
     extras_require = {
         'dev' : ['oktest', 'kook'],
         'test': ['oktest'],
@@ -35,15 +40,25 @@ Utility to download files from public CDN:
 
 * CDNJS    (https://cdnjs.com)
 * jsDelivr (https://www.jsdelivr.com)
+* UNPKG    (https://unpkg.com)
 * Google   (https://ajax.googleapis.com)
 
 Example::
 
-    $ cdnget                           # list public CDN
+    $ cdnget                           # list public CDN names
     $ cdnget cdnjs                     # list libraries
     $ cdnget cdnjs jquery              # list versions
+    $ cdnget cdnjs jquery latest       # show latest version
     $ cdnget cdnjs jquery 2.2.4        # list files
-    $ cdnget cdnjs jquery 2.2.4 /tmp   # download files
+    $ mkdir -p static/lib              # create a directory
+    $ cdnget cdnjs jquery 2.2.4 static/lib  # download files
+    $ find static/lib
+    static/lib
+    static/lib/jquery
+    static/lib/jquery/2.2.4
+    static/lib/jquery/2.2.4/jquery.min.js
+    static/lib/jquery/2.2.4/jquery.min.map
+    static/lib/jquery/2.2.4/jquery.js
 """[1:]
     #
     classifiers = [
@@ -62,6 +77,10 @@ Example::
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
     ]
